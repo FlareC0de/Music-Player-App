e47af69e823ce88256162350e3425b1ebf8afbdd
Huston Bryant <Hustonbryant22@gmail.com>
2024-01-24 22:15:38 -0500

    https://github.com/FlareC0de/Music_Player

---
+++ data.js
@@ -0,0 +1,33 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'music/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'img/cover6.png'
+    },
+    {
+        name: 'song 2',
+        path: 'music/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'img/cover7.png'
+    },
+    {
+        name: 'song 3',
+        path: 'music/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'img/cover8.png'
+    },
+    {
+        name: 'song 4',
+        path: 'music/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'img/cover9.png'
+    },
+    {
+        name: 'song 5',
+        path: 'music/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'img/cover10.png'
+    },
+
+];
